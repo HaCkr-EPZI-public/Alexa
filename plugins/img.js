be88046a6f336f1c80b5f8114c300b464cd0a489
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-11 22:32:05 +0530

    Create img.js

---
+++ plugins/img.js
@@ -0,0 +1,42 @@
+function rahf(zaytoven, denitra) {
+  const zyrie = heartlyn();
+  return rahf = function (daemen, tinishia) {
+    daemen = daemen - 385;
+    let aeries = zyrie[daemen];
+    return aeries;
+  }, rahf(zaytoven, denitra);
+}
+const dmetrius = rahf;
+(function (makensley, riccardo) {
+  const sabrine = rahf, damyra = makensley();
+  while (true) {
+    try {
+      const archita = -parseInt(sabrine(419)) / 1 * (-parseInt(sabrine(394)) / 2) + -parseInt(sabrine(392)) / 3 * (-parseInt(sabrine(395)) / 4) + parseInt(sabrine(412)) / 5 * (-parseInt(sabrine(386)) / 6) + parseInt(sabrine(408)) / 7 + parseInt(sabrine(396)) / 8 + -parseInt(sabrine(389)) / 9 + parseInt(sabrine(385)) / 10;
+      if (archita === riccardo) break; else damyra.push(damyra.shift());
+    } catch (caron) {
+      damyra.push(damyra.shift());
+    }
+  }
+}(heartlyn, 312411));
+function heartlyn() {
+  const samael = ["\n🌟 *Image ", "https://www.googleapis.com/customsearch/v1?q=", "AIzaSyDebFT-uY_f82_An6bnE9WvVcgVbzwDKgU", "13CYkeQM", "3368550BZxFcl", "3602022FVuYyu", "arraybuffer", "Please provide a search query for the image.", "5411070iqJRul", "get", "&cx=", "3VTjBLy", "sendMessage", "46348XOVZdb", "1459412rvkGWq", "2322296JvPXJR", "link", "45b94c5cef39940d1", "message", "buffer", "🖼️", "Search and send images from Google.", "length", "&searchType=image&num=5", "img", "items", "&key=", "1545047GYiUJq", "data", "No images found for your query.", "serach", "5PpUIjN", "binary", "from", "axios"];
+  heartlyn = function () {
+    return samael;
+  };
+  return heartlyn();
+}
+const {cmd} = require("../command"), axios = require(dmetrius(415)), {Buffer} = require(dmetrius(400)), GOOGLE_API_KEY = dmetrius(418), GOOGLE_CX = dmetrius(398);
+cmd({pattern: dmetrius(405), desc: dmetrius(402), react: dmetrius(401), category: dmetrius(411), filename: __filename}, async (alexxis, eivan, marquetia, {from: _0x319351, quoted: _0x507020, body: _0x580b95, isCmd: _0x835fcb, command: _0x3cde66, args: _0x532f51, q: _0x34c8d2, isGroup: _0x356eef, sender: _0x1e48c9, senderNumber: _0x39f8dd, botNumber2: _0x1ecde9, botNumber: _0x392371, pushname: _0x16c780, isMe: _0x51f667, isOwner: _0x4edb44, groupMetadata: _0x13fadb, groupName: _0x3272e1, participants: _0x43d765, groupAdmins: _0x692385, isBotAdmins: _0x14aa75, isAdmins: _0x413a6f, reply: _0x5127c4}) => {
+  const ares = dmetrius;
+  try {
+    if (!_0x34c8d2) return _0x5127c4(ares(388));
+    const andria = encodeURIComponent(_0x34c8d2), shaquasia = ares(417) + andria + ares(391) + GOOGLE_CX + ares(407) + GOOGLE_API_KEY + ares(404), quaterrius = await axios[ares(390)](shaquasia), silence = quaterrius[ares(409)];
+    if (!silence[ares(406)] || silence[ares(406)][ares(403)] === 0) return _0x5127c4(ares(410));
+    for (let taleiah = 0; taleiah < silence[ares(406)][ares(403)]; taleiah++) {
+      const chaffee = silence[ares(406)][taleiah][ares(397)], channie = await axios[ares(390)](chaffee, {responseType: ares(387)}), petyon = Buffer[ares(414)](channie[ares(409)], ares(413));
+      await alexxis[ares(393)](_0x319351, {image: petyon, caption: ares(416) + (taleiah + 1) + " from your search!* 🌟\n✨ *Epzige Cudu Nona's IMG DOWNLOADER* ✨\n        Enjoy these images! 📸\n"}, {quoted: eivan});
+    }
+  } catch (keywan) {
+    console.error(keywan), _0x5127c4("Error: " + keywan[ares(399)]);
+  }
+});
