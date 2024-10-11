e0f7cebcbf5160773f0ec85b0e87281434d6d075
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-11 22:18:20 +0530

    Update fun.js

--- plugins/fun.js
+++ plugins/fun.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👧 *Random Anime Girl Image* 👧\n> *POWERED by ASITHA-MD*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👧 *Random Anime Girl Image* 👧\n> *by Asᴍᴏᴅᴇᴜs Eᴘᴢɪ*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`Error fetching anime girl image: ${e.message}`);
@@ -69,7 +69,7 @@ async (conn, mek, m, { from, q, reply }) => {
 
 ${joke.punchline} 😄
 
-> *POWERED by Asmodeus Epzi*
+> *by Asmodeus Epzi*
 `;
 
         return reply(jokeMessage);
