219e5da706fd1a5b0105a73097e253c53e037ae6
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-12 08:20:30 +0530

    Create hack.js

---
+++ plugins/hack.js
@@ -0,0 +1,39 @@
+const taika = shanecia;
+(function (shronda, tribeca) {
+  const santi = shanecia, sharmar = shronda();
+  while (true) {
+    try {
+      const pagie = -parseInt(santi(207)) / 1 * (parseInt(santi(212)) / 2) + parseInt(santi(215)) / 3 * (-parseInt(santi(230)) / 4) + -parseInt(santi(210)) / 5 + parseInt(santi(211)) / 6 * (parseInt(santi(213)) / 7) + -parseInt(santi(202)) / 8 + parseInt(santi(222)) / 9 * (parseInt(santi(208)) / 10) + parseInt(santi(214)) / 11 * (parseInt(santi(201)) / 12);
+      if (pagie === tribeca) break; else sharmar.push(sharmar.shift());
+    } catch (aija) {
+      sharmar.push(sharmar.shift());
+    }
+  }
+}(valerieanne, 850026));
+const {cmd} = require(taika(203));
+function shanecia(amiir, sheri) {
+  const marnay = valerieanne();
+  return shanecia = function (oluwafolajimi, meeya) {
+    oluwafolajimi = oluwafolajimi - 200;
+    let maeson = marnay[oluwafolajimi];
+    return maeson;
+  }, shanecia(amiir, sheri);
+}
+function valerieanne() {
+  const latrease = ["❌ *Error:* ", "```[██████████████████████████████████████████████████] 90%``` ⏳", "7aJLupd", "6710hdtSnL", "```[██████████████████████████████████████████████] 80%``` ⏳", "496610ZeMXjX", "78YOvQpy", "215554PbhjOO", "324429BlEdlM", "1955657ZDPPsp", "3723YiwxKY", "```[████████████████████████████████████████████████████] 100%``` ✅", "🚀 *Command Execution: Complete!* 🎯", "Displays a dynamic and playful 'Hacking' message for fun.", "```[██████████████████████████] 40%``` ⏳", "🔒 *System Breach: Successful!* 🔓", "fun", "8649GZwUXW", "```[██████████] 10%``` ⏳", "```[███████████████████████████████] 50%``` ⏳", "```[█████████████████████████████████████] 60%``` ⏳", "_🕵️‍♂️ Ensuring stealth..._ 🤫", "*🔧 Finalizing operations...* 🏁", "💻 *HACK STARTING...* 💻", "```[███████████████████████] 30%``` ⏳", "5024uXhzxw", "> *EPZI-MD-HACKING-COMPLETE ☣*", "180ryxbvz", "5213312FtvEvY", "../command", "log"];
+  valerieanne = function () {
+    return latrease;
+  };
+  return valerieanne();
+}
+cmd({pattern: "hack", desc: taika(218), category: taika(221), react: "💻", filename: __filename}, async (dorotea, vichelle, khloi, {from: _0x32adef, quoted: _0x538276, body: _0x42b3d6, isCmd: _0x35e7b7, command: _0x4b64dc, args: _0x2de533, q: _0x529ce3, isGroup: _0x5af5b0, sender: _0x3d9db5, senderNumber: _0x3408b7, botNumber2: _0x3902dc, botNumber: _0x31850b, pushname: _0x47136c, isMe: _0x1eed80, isOwner: _0x248fb8, groupMetadata: _0x4c0af0, groupName: _0x1f4c1a, participants: _0x596e21, groupAdmins: _0x232898, isBotAdmins: _0x36fc47, isAdmins: _0x37c954, reply: _0x4f4e24}) => {
+  const jahnyia = taika;
+  try {
+    const akxel = [jahnyia(228), "", "*Initializing hacking tools...* 🛠️", "*Connecting to remote servers...* 🌐", "", jahnyia(223), "```[███████████████████] 20%``` ⏳", jahnyia(229), jahnyia(219), jahnyia(224), jahnyia(225), "```[██████████████████████████████████████████] 70%``` ⏳", jahnyia(209), jahnyia(206), jahnyia(216), "", jahnyia(220), jahnyia(217), "", "*📡 Transmitting data...* 📤", jahnyia(226), jahnyia(227), "", "⚠️ *Note:* All actions are for demonstration purposes only.", "⚠️ *Reminder:* Ethical hacking is the only way to ensure security.", "", jahnyia(200)];
+    for (const kamau of akxel) {
+      await dorotea.sendMessage(_0x32adef, {text: kamau}, {quoted: vichelle}), await new Promise(cing => setTimeout(cing, 1e3));
+    }
+  } catch (deletta) {
+    console[jahnyia(204)](deletta), _0x4f4e24(jahnyia(205) + deletta.message);
+  }
+});
