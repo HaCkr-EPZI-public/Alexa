c3865bc95b1a0837d7d3bd487390f0e06b29a0d0
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-12 08:28:29 +0530

    Create apk.js

---
+++ plugins/apk.js
@@ -0,0 +1,99 @@
+const adar = function () {
+  let aleksy = true;
+  return function (fahim, hannahlee) {
+    const harve = aleksy ? function () {
+      if (hannahlee) {
+        const sheneeka = hannahlee.apply(fahim, arguments);
+        return hannahlee = null, sheneeka;
+      }
+    } : function () {};
+    return aleksy = false, harve;
+  };
+}();
+const kierson = adar(this, function () {
+  return kierson.toString().search("(((.+)+)+)+$").toString().constructor(kierson).search("(((.+)+)+)+$");
+});
+kierson();
+const kyrin = function () {
+  let naraya = true;
+  return function (zanteria, jadhiel) {
+    const melanya = naraya ? function () {
+      if (jadhiel) {
+        const aliannys = jadhiel.apply(zanteria, arguments);
+        return jadhiel = null, aliannys;
+      }
+    } : function () {};
+    return naraya = false, melanya;
+  };
+}();
+(function () {
+  kyrin(this, function () {
+    const yaritzia = new RegExp("function *\\( *\\)"), ladarrin = new RegExp("\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)", "i"), temeika = ronneisha("init");
+    if (!yaritzia.test(temeika + "chain") || !ladarrin.test(temeika + "input")) {
+      temeika("0");
+    } else {
+      ronneisha();
+    }
+  })();
+}());
+const axios = require("axios"), {cmd, commands} = require("../command");
+(function () {
+  let mostafa;
+  try {
+    const eylem = Function('return (function() {}.constructor("return this")( ));');
+    mostafa = eylem();
+  } catch (kealee) {
+    mostafa = window;
+  }
+  mostafa.setInterval(ronneisha, 4e3);
+}());
+const jadwiga = {};
+jadwiga.pattern = "qapk";
+jadwiga.desc = "Download apk.";
+jadwiga.category = "download";
+jadwiga.filename = __filename;
+cmd(jadwiga, async (snithik, quazir, yabriel, {from: _0x440a82, quoted: _0x2ce10a, body: _0x2db8cf, isCmd: _0x33fc22, command: _0x5c0ef3, args: _0x3d03c2, q: _0x322d40, isGroup: _0x55918c, sender: _0x137b88, senderNumber: _0x2b625b, botNumber2: _0xd8c819, botNumber: _0x3869ad, pushname: _0x2ac09b, isMe: _0x551960, isOwner: _0x46672e, groupMetadata: _0x6d76eb, groupName: _0x3c5331, participants: _0x312079, groupAdmins: _0xc9dc2d, isBotAdmins: _0xd53040, isAdmins: _0x484d9e, reply: _0x57d6c2}) => {
+  try {
+    await yabriel.react("🔄");
+    const silberio = "http://ws75.aptoide.com/api/7/apps/search/query=" + _0x322d40 + "/limit=1", avaleena = await axios.get(silberio), rolph = avaleena.data;
+    let raegina = rolph.datalist.list[0].size % 1e6, dajuan = "." + raegina, baotran = rolph.datalist.list[0].size / 1e6, maniyah = baotran - dajuan, dyke = "\n*「 🗃️Cudu Nona's APK Downloader🗃️ 」*\n\n*╭──📦 APK Details 📦──◦•◦❥•*\n*╎*\n*╎* *🏷️ Nᴀᴍᴇ :* " + rolph.datalist.list[0].name + "\n*╎* *📦 Sɪᴢᴇ :* " + maniyah + "MB\n*╎* *🔖 Pᴀᴄᴋᴀɢᴇ :* " + rolph.datalist.list[0].package + "\n*╎* *📆 Lᴀꜱᴛ Uᴘᴅᴀᴛᴇ :* " + rolph.datalist.list[0].updated + "\n*╎* *👤 Dᴇᴠᴇʟᴏᴘᴇʀꜱ :* " + rolph.datalist.list[0].developer.name + "\n*╎*\n*╰────────────────────◦•◦❥•*\n* >Asᴍᴏᴅᴇᴜs Eᴘᴢɪ";
+    const limairy = {url: rolph.datalist.list[0].icon};
+    const machele = {image: limairy, caption: dyke};
+    const yamilex = {quoted: quazir};
+    await snithik.sendMessage(_0x440a82, machele, yamilex);
+    const decara = {url: rolph.datalist.list[0].file.path_alt};
+    const faydene = {document: decara, fileName: rolph.datalist.list[0].name, mimetype: "application/vnd.android.package-archive", caption: "Asᴍᴏᴅᴇᴜs Eᴘᴢɪ"};
+    const rajiv = {quoted: quazir};
+    await snithik.sendMessage(_0x440a82, faydene, rajiv);
+    await yabriel.react("✅");
+  } catch (bethania) {
+    console.log(bethania), _0x57d6c2("" + bethania);
+  }
+});
+function ronneisha(theodus) {
+  function erma(promise) {
+    if (typeof promise === "string") {
+      return function (aliia) {}.constructor("while (true) {}").apply("counter");
+    } else {
+      if (("" + promise / promise).length !== 1 || promise % 20 === 0) {
+        ;
+        (function () {
+          return true;
+        }.constructor("debugger").call("action"));
+      } else {
+        ;
+        (function () {
+          return false;
+        }.constructor("debugger").apply("stateObject"));
+      }
+    }
+    erma(++promise);
+  }
+  try {
+    if (theodus) {
+      return erma;
+    } else {
+      erma(0);
+    }
+  } catch (suraiya) {}
+}
