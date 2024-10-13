2819203b4439513a307a2e42317102eb35fd6293
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-13 16:11:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,8 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "7d8FmS7I#Lnt4Lz46fVugIM78Yu382EzeZ6dep3l5H4ZQ3yof3Ko",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MONGODB: process.env.MONGODB || "mongodb+srv://epzi:e1IX5RIPcfb0aT2t@epzi0.taajc.mongodb.net/",
-OMDB_API_KEY: process.env.OMDB_API_KEY || "a8f3fe20",    
+OMDB_API_KEY: process.env.OMDB_API_KEY || "a8f3fe20",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 };
