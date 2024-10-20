60090681cc2b0ce6dc1ef44b5a4e0ed9b2454652
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-20 17:00:30 +0530

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -1,255 +1,131 @@
-// Alexa Whatsapp Bot....Created By Asᴍᴏᴅᴇᴜs Eᴘᴢɪ
-// Don't Edit This Fill
+const {
+  command,
+  isPrivate,
+  isUrl,
+  AddMp3Meta,
+  getBuffer,
+  toAudio,
+  getJson,
+  validateQuality,
+} = require("../../lib");
+const { yta, ytv, ytsdl } = require("../../lib/ytdl");
 
-const {cmd , commands} = require('../command')
-const fg = require('api-dylux')
-const yts = require('yt-search')
+command(
+  {
+    pattern: "yta",
+    fromMe: isPrivate,
+    desc: "Download audio from youtube",
+  },
+  async (message, match) => {
+    match = match || message.reply_message.text;
+    if (!match) return await message.reply("Give me a youtube link");
+    if (!isUrl(match)) return await message.reply("Give me a youtube link");
+    let { dlink, title } = (
+      await getJson(
+        `https://api.thexapi.xyz/api/v1/download/youtube/audio?url=${match}`
+      )
+    ).data;
+    await message.reply(`_Downloading ${title}_ \n\n >©ᴄʀᴇᴀᴛᴇᴅ ʙʏ Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 🧑🏻‍💻`);
+    let buff = await getBuffer(dlink);
+    return await message.sendMessage(
+      message.jid,
+      buff,
+      {
+        mimetype: "audio/mpeg",
+        filename: title + ".mp3",
+      },
+      "audio"
+    );
+  }
+);
 
-//______________________________________SONG-DOWNLODER_______________________________
+command(
+  {
+    pattern: "ytv",
+    fromMe: isPrivate,
+    desc: "Download audio from youtube",
+  },
+  async (message, match) => {
+    match = match || message.reply_message.text;
+    let url = getUrl(match)[0];
+    if (!url)
+      return await message.reply(
+        "Give me a youtube link\n\nExample: ytv youtube.com/watch?v=xxxxx 480p"
+      );
+    let quality = match.split(";")[1];
+    if (quality && !validateQuality(quality)) {
+      return await message.reply(
+        "Invalid Resolution \nSupported: 144p, 240p, 360p, 480p, 720p, 1080p, 1440p, 2160p"
+      );
+    } else if (!quality) quality = "360p";
+    if (!match)
+      return await message.reply(
+        "Give me a youtube link\n\nExample: ytv youtube.com/watch?v=xxxxx 480p"
+      );
+    if (!isUrl(match))
+      return await message.reply(
+        "Give me a youtube link\n\nExample: ytv youtube.com/watch?v=xxxxx 480p"
+      );
+    let requrl = `https://api.thexapi.xyz/api/v1/download/youtube/video?url=${url}&quality=${quality}`;
+    let response = (await getJson(requrl)).data;
+    const { dlink, title } = response;
+    console.log(response);
+    await message.reply(`_Downloading ${title}_ \n\n >©ᴄʀᴇᴀᴛᴇᴅ ʙʏ Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 🧑🏻‍💻`);
+    return await message.sendMessage(
+      message.jid,
+      dlink,
+      {
+        mimetype: "video/mp4",
+        filename: title + ".mp4",
+      },
+      "video"
+    );
+  }
+);
 
-cmd({
+command(
+  {
     pattern: "song",
-    desc: "downlode songs",
-    category: "downlode",
-    react: "🎵",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if(!q) return reply("*Please give me a YT url or Title*")
-const search = await yts(q)
-const data = search.videos[0];
-const url = data.url
+    fromMe: isPrivate,
+    desc: "Download audio from youtube",
+  },
+  async (message, match) => {
+    match = match || message.reply_message.text;
+    if (!match) return await message.reply("Give me a query");
+    let { dlink, title } = await ytsdl(match);
+    await message.reply(`_Downloading ${title}_ \n\n >©ᴄʀᴇᴀᴛᴇᴅ ʙʏ Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 🧑🏻‍💻`);
+    let buff = await getBuffer(dlink);
+    return await message.sendMessage(
+      message.jid,
+      buff,
+      {
+        mimetype: "audio/mpeg",
+        filename: title + ".mp3",
+      },
+      "audio"
+    );
+  }
+);
 
-let desc = `
-*──────────────────*
-_*🎧 ＳＯＮＧ ＤＯＷＮＬＯＤＥＲ 🎧*_
-*──────────────────*
-
-┌────────────────
-│✑ *Title:* _${data.title}_
-│
-│✑ *Time:* _${data.timestamp}_
-│
-│✑ *Ago:* _${data.ago}_
-│
-│✑ *Views:* _${data.views}_
-└────────────────
-
-*┌────────────────*
-_📤ᴜᴘʟᴏᴅɪɴɢ ʏᴏᴜʀ ᴀᴜᴅɪᴏ ꜰɪʟʟ📤_
-*└────────────────*
-
-> *ᴄʀᴇᴀᴛᴇᴅ ʙʏ Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 🧑🏻‍💻*
-`
-
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
-
-//________________________________Downlode-Audio_______________________________________
-
-let down = await fg.yta(url)
-let downloadUrl = down.dl_url
-
-//________________________________Send-Audio_____________________________________________
-
-await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
-
-}catch(e){
-    console.log(e)
-    reply(`${e}`)
-}
-}
-)
-
-//_________________________________VIDEO-DOWNLODER_____________________________________
-
-cmd({
+command(
+  {
     pattern: "video",
-    desc: "downlode videos",
-    category: "downlode",
-    react: "🎬",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if(!q) return reply("*Please give me a YT url or Title*")
-const search = await yts(q)
-const data = search.videos[0];
-const url = data.url
-
-let desc = `
-*──────────────────*
-_*🎬 VIDEO ＤＯＷＮＬＯＤＥＲ 🎬*_
-*──────────────────*
-
-┌────────────────
-│✑ *Title:* _${data.title}_
-│
-│✑ *Time:* _${data.timestamp}_
-│
-│✑ *Ago:* _${data.ago}_
-│
-│✑ *Views:* _${data.views}_
-└────────────────
-
-*┌────────────────*
-_📤ᴜᴘʟᴏᴅɪɴɢ ʏᴏᴜʀ Video ꜰɪʟʟ📤_
-*└────────────────*
-
-> *ᴄʀᴇᴀᴛᴇᴅ ʙʏ Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 🧑🏻‍💻*
-`
-
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
-
-//________________________________Downlode-Video_______________________________________
-
-let down = await fg.ytv(url)
-let downloadUrl = down.dl_url
-
-//________________________________Send-Video_____________________________________________
-
-await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4",caption:"*©ᴄʀᴇᴀᴛᴇᴅ ʙʏ Asᴍᴏᴅᴇᴜs Eᴘᴢɪ..🧑🏻‍💻*"},{quoted:mek})
-
-
-}catch(e){
-    console.log(e)
-    reply(`${e}`)
-}
-}
-)
-cmd(
-  {
-    pattern: 'play',
-    alias: ['music'],
-    react: '🎧',
-    desc: 'Search and get details from YouTube.',
-    category: 'download',
-    filename: __filename,
+    fromMe: isPrivate,
+    desc: "Download video from youtube",
   },
-  async (
-    client,
-    message,
-    chat,
-    {
-      from,
-      body,
-      quoted,
-      isCmd,
-      sender,
-      args,
-      searchQuery,
-      isGroup,
-      senderNumber,
-      botNumber2,
-      botNumber,
-      pushname,
-      isMe,
-      isOwner,
-      groupMetadata,
-      groupName,
-      participants,
-      groupAdmins,
-      isBotAdmins,
-      isAdmins,
-      reply,
-    }
-  ) => {
-    try {
-      if (!searchQuery) {
-        return reply('*Please provide search terms*')
-      }
-      const searchResult = await yts(searchQuery),
-        video = searchResult.videos[0],
-        videoUrl = video.url
-      let audioData = await fg.yta(videoUrl),
-        downloadUrl = audioData.dl_url
-      reply('*🎸 Downloading... 🎸*\n> Asᴍᴏᴅᴇᴜs Eᴘᴢɪ')
-      let response = await client.sendMessage(
-        from,
-        {
-          document: { url: downloadUrl },
-          mimetype: 'audio/mpeg',
-          fileName: video.title + '.mp3',
-          caption: 'Created by Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 👩🏻‍💻',
-        },
-        { quoted: message }
-      )
-      await client.sendMessage(from, {
-        react: {
-          text: '✅',
-          key: response.key,
-        },
-      })
-    } catch (error) {
-      console.log(error)
-      reply('' + error)
-    }
+  async (message, match) => {
+    match = match || message.reply_message.text;
+    if (!match) return await message.reply("Give me a query");
+    let { dlink, title } = await ytsdl(match, "video");
+    await message.reply(`_Downloading ${title}_ \n\n >©ᴄʀᴇᴀᴛᴇᴅ ʙʏ Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 🧑🏻‍💻`);
+    return await message.sendMessage(
+      message.jid,
+      dlink,
+      {
+        mimetype: "video/mp4",
+        filename: title + ".mp4",
+      },
+      "video"
+    );
   }
-)
-
-cmd(
-  {
-    pattern: 'yta',
-    alias: ['ytmp3'],
-    react: '🎧',
-    desc: 'Download YouTube videos as audio.',
-    category: 'download',
-    filename: __filename,
-  },
-  async (
-    client,
-    message,
-    chat,
-    {
-      from,
-      body,
-      quoted,
-      isCmd,
-      sender,
-      args,
-      searchQuery,
-      isGroup,
-      senderNumber,
-      botNumber2,
-      botNumber,
-      pushname,
-      isMe,
-      isOwner,
-      groupMetadata,
-      groupName,
-      participants,
-      groupAdmins,
-      isBotAdmins,
-      isAdmins,
-      reply,
-    }
-  ) => {
-    try {
-      if (!searchQuery) {
-        return reply('*Please provide search terms*')
-      }
-      let audioData = await fg.yta(searchQuery),
-        downloadUrl = audioData.dl_url
-      reply('*🎸 Downloading... 🎸*\n> Asᴍᴏᴅᴇᴜs Eᴘᴢɪ')
-      let response = await client.sendMessage(
-        from,
-        {
-          document: { url: downloadUrl },
-          mimetype: 'audio/mpeg',
-          fileName: data.title + '.mp3',
-          caption: 'Created by Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 👩🏻‍💻',
-        },
-        { quoted: message }
-      )
-      await client.sendMessage(from, {
-        react: {
-          text: '✅',
-          key: response.key,
-        },
-      })
-    } catch (error) {
-      console.log(error)
-      reply('' + error)
-    }
-  }
-)
+);
