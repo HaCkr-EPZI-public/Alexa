e2d9f79566571ff4717e0d4fe461a97828bdbc82
Epzi <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-10-20 16:11:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "7d8FmS7I#Lnt4Lz46fVugIM78Yu382EzeZ6dep3l5H4ZQ3yof3Ko",
+SESSION_ID: process.env.SESSION_ID || "PENAWD6Y#Jp2DIlAMY61ojXJC3Jax0-RijJykuPLFUaWeKYdoMQI",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MONGODB: process.env.MONGODB || "mongodb+srv://epzi:e1IX5RIPcfb0aT2t@epzi0.taajc.mongodb.net/",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "a8f3fe20",
